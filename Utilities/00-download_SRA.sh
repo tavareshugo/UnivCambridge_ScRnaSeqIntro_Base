46e3db0ce6b50a8009423f5b34b57f515da1a350
Ashley Sawle <Ashley.Sawle@cruk.cam.ac.uk>
2022-07-26 10:10:27 +0100

    Modified to use SraRunTable.txt

--- Utilities/00-download_SRA.sh
+++ Utilities/00-download_SRA.sh
@@ -1,47 +1,36 @@
 #!/bin/bash
-#SBATCH -A LEYSER-SL2-CPU
-#SBATCH -D /rds/user/hm533/hpc-work/scRNA-seq_course/make_data
 #SBATCH -J public_data
-#SBATCH -o logs/00-download_SRA_%a.log
+#SBATCH -o download_SRA.%j_%a.log
+#SBATCH -e download_SRA.%j_%a.err
 #SBATCH -c 1
 #SBATCH -t 12:00:00
-#SBATCH -p cclake,cclake-himem
-#SBATCH -a 2-3 # rows in the CSV file with SRA information
+#SBATCH --mem 2G
+#SBATCH -a 2-13 # rows in the CSV file with SRA informatioj
+
+set -ex
 
 # activate conda environment
 source activate sra
 
 # get sample information from the table
-INFO=$(head -n "$SLURM_ARRAY_TASK_ID" sample_info.csv | tail -n 1)
+ID=`head -n $SLURM_ARRAY_TASK_ID SraRunTable.txt | tail -n 1 | cut -d "," -f 1`
 
-# split relevant parts
-ID=$(echo $INFO | cut -d "," -f 1)
-SAMPLE=$(echo $INFO | cut -d "," -f 2)
-TYPE=$(echo $INFO | cut -d "," -f 3)
-LINK=$(echo $INFO | cut -d "," -f 5)
-
-# move to data folder
-mkdir -p data/reads/
-cd data/reads/
-
-# create directory structure
-OUTDIR="${SAMPLE}/"
-mkdir -p "$OUTDIR"
-cd "$OUTDIR"
+# create data folders
+sraDir=data/sra
+mkdir -p ${sraDir}
+fqDir=data/fastq
+mkdir -p ${fqDir}
 
 # download file
-wget -O "${ID}.sra" "$LINK"
+prefetch --max-size 30G -O ${sraDir} ${ID}
 
 # convert to fastq
-fastq-dump -O . --gzip --split-files "${ID}.sra"
+fastq-dump -O ${fqDir} --gzip --split-files ${sraDir}/${ID}/${ID}.sra
 
 # rename files for cellranger
 # 1 = I1
 # 2 = R1
 # 3 = R2
-mv "${ID}_1.fastq.gz" "${ID}_S1_L001_I1_001.fast.gz"
-mv "${ID}_2.fastq.gz" "${ID}_S1_L001_R1_001.fast.gz"
-mv "${ID}_3.fastq.gz" "${ID}_S1_L001_R2_001.fast.gz"
-
-# remove original file
-rm "${ID}.sra"
+mv ${fqDir}/${ID}_1.fastq.gz ${fqDir}/${ID}_S1_L001_I1_001.fast.gz
+mv ${fqDir}/${ID}_2.fastq.gz ${fqDir}/${ID}_S1_L001_R1_001.fast.gz
+mv ${fqDir}/${ID}_3.fastq.gz ${fqDir}/${ID}_S1_L001_R2_001.fast.gz
