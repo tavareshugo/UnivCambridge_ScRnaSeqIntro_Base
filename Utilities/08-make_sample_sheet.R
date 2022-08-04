9a3861b4dbd7fe02a3517bed3ca725a363d37d18
Ashley Sawle <Ashley.Sawle@cruk.cam.ac.uk>
2022-08-04 09:48:05 +0100

    Change column names

--- Utilities/08-make_sample_sheet.R
+++ Utilities/08-make_sample_sheet.R
@@ -3,9 +3,9 @@
 library(tidyverse)
 
 read_csv("data/SraRunTable.txt") %>%
-    select(SampleID = Run, SampleGroup = source_name) %>%
-    arrange(SampleID) %>%
-    mutate(BioSample = SampleID) %>%
+    select(Sample = Run, SampleGroup = source_name) %>%
+    arrange(Sample) %>%
+    mutate(BioSample = Sample) %>%
     mutate(BioSample = ifelse(BioSample=="SRR9264352", 
                               "SRR9264351", 
                               BioSample)) %>%
@@ -14,8 +14,8 @@ read_csv("data/SraRunTable.txt") %>%
     ungroup() %>%
     mutate(BioSample = str_c(SampleGroup, "_", Rep)) %>%
     mutate(SampleName = case_when(
-           SampleID == "SRR9264351" ~ str_c(BioSample, "a"),
-           SampleID == "SRR9264352" ~ str_c(BioSample, "b"),
+           Sample == "SRR9264351" ~ str_c(BioSample, "a"),
+           Sample == "SRR9264352" ~ str_c(BioSample, "b"),
            TRUE ~ BioSample)) %>%
-    select(SampleID, BioSample, SampleName, SampleGroup) %>%
+    select(Sample, BioSample, SampleName, SampleGroup) %>%
     write_tsv("data/sample_sheet.tsv")
