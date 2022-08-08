11a127f7ad7e8f206b5eb1cc6671c761e9a213c4
Ashley Sawle <Ashley.Sawle@cruk.cam.ac.uk>
2022-08-08 11:00:04 +0100

    Working on DM

---
+++ Utilities/11-Dimension_Reduction.R
@@ -0,0 +1,64 @@
+#!/usr/bin/env Rscript
+#SBATCH -J DimensionReduction
+#SBATCH -o DimensionReduction.%j.out
+#SBATCH -e DimensionReduction.%j.err
+#SBATCH --mincpus 8 
+#SBATCH --mem=16G
+#SBATCH --time=02:57:42
+
+# This script performs feature selection and then runs PCA,
+# tSNE and UMAP.
+
+library(scater) 
+library(scran)
+
+## Full data set 
+# Load data
+sce <- readRDS("data/R_objects/Caron_normalized.full.rds")
+
+# use common gene names instead of Ensembl gene IDs
+rownames(sce) <- uniquifyFeatureNames(rownames(sce), rowData(sce)$Symbol)
+
+# Identify highly variable genes
+gene_var <- modelGeneVar(sce)
+hvgs <- getTopHVGs(gene_var, prop=0.1)
+
+# PCA 
+sce <- runPCA(sce, subset_row = hvgs)
+
+# tSNE
+sce <- runTSNE(sce, dimred="PCA", n_dimred=10)
+
+# UMAP
+
+sce <- runUMAP(sce, dimred="PCA", n_dimred=10)
+
+# save object
+
+saveRDS(sce, "data/R_objects/Caron_dimRed.full.rds")
+
+## 500 cell/sample data set
+# Load data
+sce <- readRDS("data/R_objects/Caron_normalized.500.rds")
+
+# use common gene names instead of Ensembl gene IDs
+rownames(sce) <- uniquifyFeatureNames(rownames(sce), rowData(sce)$Symbol)
+
+# Identify highly variable genes
+gene_var <- modelGeneVar(sce)
+hvgs <- getTopHVGs(gene_var, prop=0.1)
+
+# PCA
+sce <- runPCA(sce, subset_row = hvgs)
+
+# run denoise PCA step
+sce <- denoisePCA(sce, technical = gene_var)
+
+# tSNE
+sce <- runTSNE(sce, dimred="PCA", n_dimred=10)
+
+# UMAP
+sce <- runUMAP(sce, dimred="PCA", n_dimred=10)
+
+# save object
+saveRDS(sce, "data/R_objects/Caron_dimRed.500.rds")
