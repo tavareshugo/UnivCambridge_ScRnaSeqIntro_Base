3f5bf65c8632d66c18707bab76957cd7ef927b3b
Ashley Sawle <Ashley.Sawle@cruk.cam.ac.uk>
2022-09-08 10:06:47 +0100

    Make new sample sheet omitting tech rep

--- Utilities/08-make_sample_sheet.R
+++ Utilities/08-make_sample_sheet.R
@@ -2,20 +2,15 @@
 
 library(tidyverse)
 
+# We are going to omit the technical replicate for PBMMC_1 - SRR9264352
+
 read_csv("data/SraRunTable.txt") %>%
     select(Sample = Run, SampleGroup = source_name) %>%
+    filter(Sample != "SRR9264352") %>%
     arrange(Sample) %>%
-    mutate(BioSample = Sample) %>%
-    mutate(BioSample = ifelse(BioSample=="SRR9264352", 
-                              "SRR9264351", 
-                              BioSample)) %>%
     group_by(SampleGroup) %>%
-    mutate(Rep = as.numeric(as.factor(BioSample))) %>%
+    mutate(Rep = as.numeric(as.factor(Sample))) %>%
     ungroup() %>%
-    mutate(BioSample = str_c(SampleGroup, "_", Rep)) %>%
-    mutate(SampleName = case_when(
-           Sample == "SRR9264351" ~ str_c(BioSample, "a"),
-           Sample == "SRR9264352" ~ str_c(BioSample, "b"),
-           TRUE ~ BioSample)) %>%
-    select(Sample, BioSample, SampleName, SampleGroup) %>%
+    mutate(SampleName = str_c(SampleGroup, "_", Rep)) %>%
+    select(Sample, SampleName, SampleGroup) %>%
     write_tsv("data/sample_sheet.tsv")
