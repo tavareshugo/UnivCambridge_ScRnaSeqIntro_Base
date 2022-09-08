a6f8e3de631147d6128841ea8dcf026002141c65
Ashley Sawle <Ashley.Sawle@cruk.cam.ac.uk>
2022-09-08 12:57:58 +0100

    Make from scratch as 1b is not in filtered object

--- Utilities/12-Make_dataset_intergration_PBMMC_1_objects.R
+++ Utilities/12-Make_dataset_intergration_PBMMC_1_objects.R
@@ -6,47 +6,88 @@
 #SBATCH --mem=16G
 #SBATCH --time=02:57:42
 
-# This script starts with the all cells filtered data set. It extracts the two
-# technical replicate PBMMC samples - SRR9264351 & SRR9264352. It then
-# normalizes the counts, runs dimension reduction and clustering (default of
-# clusterCells) on each of these samples separately.
+# This script loads the two technical replicate PBMMC samples - SRR9264351 &
+# SRR9264352. It then filteres the cells, normalizes the counts, runs dimension
+# reduction and clustering (default of clusterCells) on each of these samples
+# separately.
 
+library(DropletUtils)
 library(scater) 
 library(scran)
 library(BiocParallel)
+library(rtracklayer)
+library(tidyverse)
 
-bpp <- MulticoreParam(8)
+bp.params <- MulticoreParam(workers = 8)
 
-## Full data set 
-# Load data
-sce <- readRDS("data/R_objects/Caron_filtered.full.rds")
+prepSample <- function(sampleID){
+    # load data
+    cr_dir <- str_c("data/cellranger/", 
+                    sampleID, 
+                    "/outs/filtered_feature_bc_matrix")
+    names(cr_dir) <- sampleID
+    sce <- read10xCounts(cr_dir, col.names=TRUE, BPPARAM = bp.params)
 
-# Subset the object
+    # add meta data 
+    sce$Barcode <- rownames(colData(sce))
+    sce$SampleName <- ifelse(sampleID == "SRR9264351", "PBMMC_1a", "PBMMC_1b") 
+    sce$SampleGroup <- "PBMMC"
 
-prepSample <- function(sampleID, sceObj = sce){
-    subsce <- sceObj[, sceObj$Sample==sampleID]
+    # Keep only detected genes
+    detected_genes <- rowSums(counts(sce)) > 0
+    sce <- sce[detected_genes,]
+
+    # Annotate genes
+
+    # or just get the info from the GTF
+    gtf <- "data/references/refdata-gex-GRCh38.p13-Gencode.v41/genes/genes.gtf.gz"
+    annot <- readGFF(gtf, 
+                    columns="seqid", 
+                    tags="gene_id", 
+                    filter=list(type="gene")) %>%
+        rename(ID = gene_id, Chromosome = seqid)
+
+    rowData(sce) <- merge(rowData(sce), annot, by = "ID", sort=FALSE)
+    rownames(rowData(sce)) <- rowData(sce)$ID
+
+
+    # qc addPerCellQC
+    is.mito <- rowData(sce)$Chromosome%in%c("chrM", "MT")
+    sce <- addPerCellQC(sce, subsets=list(Mito=is.mito), BPPARAM = bp.params)
+
+
+    # adpative thresholds with Sample as batch
+    cell_qc_results <- quickPerCellQC(colData(sce),
+                                    percent_subsets=c("subsets_Mito_percent"),
+                                    batch=sce$Sample)
+
+    sce <- sce[, !cell_qc_results$discard]
+
     # Remove the per cell QC metrics
-    colData(subsce) <- colData(subsce)[,1:5]
+    colData(sce) <- colData(sce)[,1:4]
+
     # Remove undetected genes
-    keep <- rowSums(counts(subsce)) > 0
-    subsce <- subsce[keep,]
+    keep <- rowSums(counts(sce)) > 0
+    sce <- sce[keep,]
+
     # Normalize the counts
     set.seed(100) 
-    clust <- quickCluster(subsce) 
-    subsce <- computePooledFactors(subsce, cluster=clust, min.mean=0.1)
-    subsce <- logNormCounts(subsce)
+    clust <- quickCluster(sce) 
+    sce <- computePooledFactors(sce, cluster=clust, min.mean=0.1)
+    sce <- logNormCounts(sce)
+
     # Identify highly variable genes
-    gene_var <- modelGeneVar(subsce)
+    gene_var <- modelGeneVar(sce)
     hvgs <- getTopHVGs(gene_var, prop=0.1)
-    # PCA 
-    subsce <- runPCA(subsce, subset_row = hvgs)
-    # tSNE
-    subsce <- runTSNE(subsce, dimred="PCA", n_dimred=10, BPPARAM = bpp)
-    # UMAP
-    subsce <- runUMAP(subsce, dimred="PCA", n_dimred=10, BPPARAM = bpp)
+
+    # Dimension reduction
+    sce <- runPCA(sce, subset_row = hvgs)
+    sce <- runTSNE(sce, dimred="PCA", n_dimred=10, BPPARAM = bp.params)
+    sce <- runUMAP(sce, dimred="PCA", n_dimred=10, BPPARAM = bp.params)
+
     # Clustering
-    colLabels(subsce) <- clusterCells(subsce, use.dimred = "PCA")
-    subsce
+    colLabels(sce) <- clusterCells(sce, use.dimred = "PCA")
+    sce
 }
 
 pbmmc1a <- prepSample("SRR9264351")
