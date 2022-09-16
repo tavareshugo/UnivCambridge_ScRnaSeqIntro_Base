1c818e4a15e1b760bacd9e64b2417e7a856a2006
Ashley Sawle <ads2202cu@gmail.com>
2022-09-16 09:43:40 +0100

    Dataset Intergration worksheet

---
+++ Markdowns/07_Dataset_Integration_Exercise.R
@@ -0,0 +1,26 @@
+# Dataset Integration Exercise worksheet
+library(scater)
+library(scran)
+library(batchelor)
+library(bluster)
+library(pheatmap)
+library(magrittr)
+set.seed(1704)
+
+# load the data
+sce_all <- readRDS("R_objects/Caron_dimRed.500.rds")
+
+# tabulate the number of cells per sample
+table(sce_all$SampleName)
+
+### Add your own code to complete the following steps
+
+# obtain a batch-corrected SCE object
+
+# add the corrected matrix to the original object - to keep it all together
+
+# add a tSNE using the corrected data
+
+# visualise uncorrected tSNE
+
+# visualise corrected tSNE
